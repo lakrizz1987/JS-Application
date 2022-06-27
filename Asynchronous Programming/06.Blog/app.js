cc29fdc9919b71d2951f4c88e730566a766064ea
Ivaylo <ignatov.ivaylo@gmail.com>
2022-06-27 16:35:21 +0300

    commit

--- Asynchronous Programming/06.Blog/app.js
+++ Asynchronous Programming/06.Blog/app.js
@@ -2,14 +2,11 @@ function attachEvents() {
     let loadBtnElement = document.getElementById('btnLoadPosts');
     let viewBtnElement = document.getElementById('btnViewPost');
 
-    let selectElement = document.getElementById('posts');
 
-    loadBtnElement.addEventListener('click', generate);
 
-    function generate(){
-        let data = loadPosts()
-        console.log(data)
-    }
+    loadBtnElement.addEventListener('click', loadPosts);
+    viewBtnElement.addEventListener('click', getComments);
+
 
 
 
@@ -21,18 +18,38 @@ function attachEvents() {
 }
 
 attachEvents();
-
+let info = {};
 async function loadPosts() {
+    let selectElement = document.getElementById('posts');
     let response = await fetch('http://localhost:3030/jsonstore/blog/posts');
     let data = await response.json();
-    return data;
+
+    Object.values(data).forEach(obj => {
+        let option = create('option', `${obj.title}`, selectElement);
+        option.value = obj.id;
+        info[obj.id] = [obj.body, obj.title];
+    })
+
+}
+
+function getComments() {
+    let selectElement = document.getElementById('posts');
+    let id = selectElement.value;
+    getCommentsByID(id)
 }
 
 async function getCommentsByID(id) {
     let response = await fetch('http://localhost:3030/jsonstore/blog/comments');
     let data = await response.json();
-    let array = Object.values(data).filter(x => x.id == id);
-    return array;
+    let array = Object.values(data).filter(x => x.postId == id);
+
+    document.getElementById('post-title').textContent = info[id][1];
+    document.getElementById('post-body').textContent = info[id][0];
+    let ul = document.getElementById('post-comments');
+    ul.innerHTML = '';
+    array.forEach(x => {
+        let li = create('li', `${x.text}`, ul)
+    })
 }
 
 function create(type, content, parent) {
