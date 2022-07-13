3e8f3721c2c894f68380077c8d07d23345d2ab7c
Ivaylo <ignatov.ivaylo@gmail.com>
2022-07-13 15:43:37 +0300

    commit login

--- Architecture and Testing EX/SoftTerest/login.js
+++ Architecture and Testing EX/SoftTerest/login.js
@@ -1,5 +1,27 @@
-let sectionLogin = document.getElementById('loginPage');
+import { post } from "./api.js";
+import { showHome } from "./home.js";
 
-export function showLogin(section){
+let sectionLogin = document.getElementById('loginPage');
+let form = document.getElementById('login-form');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    let data = new FormData(form);
+
+    let email = data.get('email')
+    let password = data.get('password');
+
+    let user = await post('http://localhost:3030/users/login', { email, password });
+    let userData = JSON.stringify(user)
+    localStorage.setItem('user', userData);
+
+    let main = document.querySelector('main');
+    main.replaceChildren()
+
+    showHome(main)
+});
+
+export function showLogin(section) {
     section.replaceChildren(sectionLogin);
 }
+
